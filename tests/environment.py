84d7a075ff67e084738e907b685e05e9f8e48feb
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-04-24 14:44:58 +0200

    Don't trap exceptions in before_/after_all - this is now correctly handled by behave

--- tests/environment.py
+++ tests/environment.py
@@ -14,30 +14,24 @@ def before_scenario(context, scenario):
     # Can be redefined in runtime via 'behave tests -D image="woot"'
     # If its not specified it will be built
 
-    try:
-        if 'image' not in context.config.userdata:
-            raise Exception("Please specify image to test: behave tests -D=image=test")
-        context.image = context.config.userdata['image']
+    if 'IMAGE' not in context.config.userdata:
+        raise Exception("Please specify image to test: behave tests -D=IMAGE=test")
+    context.image = context.config.userdata['IMAGE']
 
-        # Make sure we generate nice filename here (for images like openshift/postgresql-92-centos7)
-        cid_file_name = re.sub(r'\W+', '', context.image)
-        context.cid_file = "/tmp/%s.cid" % cid_file_name
+    # Make sure we generate nice filename here (for images like openshift/postgresql-92-centos7)
+    cid_file_name = re.sub(r'\W+', '', context.image)
+    context.cid_file = "/tmp/%s.cid" % cid_file_name
 
-        docker_cleanup(context)
-    except Exception as e:
-        print("before_scenario: exception %s" % str(e))
+    docker_cleanup(context)
 
 
 def after_scenario(context, scenario):
     # TODO: Move me to a function and run it from example's before_scenario
 
     # Store scenario logs
-    try:
-        if not getattr(context, "cid", None):
-            return
-        if scenario.status == 'failed':
-            run("docker logs %s" % context.cid)
+    if not getattr(context, "cid", None):
+        return
+    if scenario.status == 'failed':
+        run("docker logs %s" % context.cid)
 
-        docker_cleanup(context)
-    except Exception as e:
-        print("after_scenario: exception %s" % str(e))
+    docker_cleanup(context)
